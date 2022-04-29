8087b0bc3197bc675c2d62c99a334241412b1fe4
Oluwagbemi Oluwasakin <51876261+Gbemi06@users.noreply.github.com>
2022-04-29 14:17:25 +0300

    CRUD on reviews done

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import productRouter from "./services/products/index.js";
 import listEndpoints from "express-list-endpoints";
+import reviewRouter from "./services/reviews/index.js";
 
 const server = express();
 const port = process.env.PORT || 3000;
@@ -15,12 +16,13 @@ server.use(express.json());
 //endpoints ----------------------------------------------------------------
 
 server.use("/product", productRouter);
+server.use("/reviews", reviewRouter);
 
 //connection to db-----------------------------------------------------------
 
 mongoose.connect(process.env.MONGO_CONNECTION);
 mongoose.connection.on("connected", () => {
-  console.log("successifully connected"),
+  console.log("successfully connected"),
     server.listen(port, () => {
       console.table(listEndpoints(server));
       console.log(`server is running on port: ${port}`);
---
+++ src/services/reviews/model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const reviewSchema = new Schema(
+  {
+    comment: { type: String, required: true },
+    rate: { type: Number, min: 1, max: 5, required: true },
+    product: [{ type: Schema.Types.ObjectId, ref: "Products" }],
+  },
+  {
+    timestamps: true,
+    updatedAt: false,
+  }
+);
+
+export default model("reviews", reviewSchema);
